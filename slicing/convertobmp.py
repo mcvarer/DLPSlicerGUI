6cdc8c37fc94b2c6fef3e43b56a742998c5e0a38
Kevin Wan <114878518+KevinWan123@users.noreply.github.com>
2022-11-09 14:45:18 -0500

    update

--- slicing/convertobmp.py
+++ slicing/convertobmp.py
@@ -41,43 +41,43 @@ def BMP(folder_name, Resolution,height,width,dpi,Reverse =False):
             #img.save('./{}/example{}.bmp'.format(folder_name,z),dpi = (dpi,dpi))
         
     else:
-        # try:
-        for z in range(0,len(Resolution)):
-            count+=1
-            Size = np.zeros((width,height)).tolist()
-            img = Image.new('1', (width, height))
-            pixels = img.load()
-            dpi = int(dpi)
+        try:
+            for z in range(0,len(Resolution)):
+                count+=1
+                Size = np.zeros((width,height)).tolist()
+                img = Image.new('1', (width, height))
+                pixels = img.load()
+                dpi = int(dpi)
 
 
-            currentRes = Resolution[z].tolist()
-            print(len(Resolution[z]),len(Resolution[0][z]))
+                currentRes = Resolution[z].tolist()
+                print(len(Resolution[z]),len(Resolution[0][z]))
 
-        
-            pointList = []
-            print(len(Size))
-            for y in range(0,len(currentRes)):
-                for x in range(0,len(currentRes[0])):
-                    if currentRes[y][x]==True:
-                        pointList.append((y+int(gWidth-10),x+int(gHeight-10))) 
-                        #IF object is too big, it might mess with the centering.
             
-            for i in pointList:
-                Size[i[0]][i[1]]= 255
+                pointList = []
+                print(len(Size))
+                for y in range(0,len(currentRes)):
+                    for x in range(0,len(currentRes[0])):
+                        if currentRes[y][x]==True:
+                            pointList.append((y+int(gWidth-10),x+int(gHeight-10))) 
+                            #IF object is too big, it might mess with the centering.
+                
+                for i in pointList:
+                    Size[i[0]][i[1]]= 255
+                
+
+                            
             
 
-                        
-        
-
-            data = Size
-            for i in range(img.size[0]): # each list
-                for j in range(img.size[1]): # each element of 
-                    pixels[i, j] = int(data[i][j])
-        
-            img.save('./{}/example{}.bmp'.format(folder_name,count), dpi = (dpi,dpi)) 
-            #img.save('./{}/example{}.bmp'.format(folder_name,z),dpi = (dpi,dpi))
-        # except:
-        #     pass
+                data = Size
+                for i in range(img.size[0]): # each list
+                    for j in range(img.size[1]): # each element of 
+                        pixels[i, j] = int(data[i][j])
+            
+                img.save('./{}/example{}.bmp'.format(folder_name,count), dpi = (dpi,dpi)) 
+                #img.save('./{}/example{}.bmp'.format(folder_name,z),dpi = (dpi,dpi))
+        except:
+            pass
 
             
 
